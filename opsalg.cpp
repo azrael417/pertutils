9ccebdf02139f8c788816dd5b7c18b823bf20a8a
Thorsten Kurth <thorstenkurth@me.com>
2013-12-17 16:05:28 -0800

    debugging

--- opsalg.cpp
+++ opsalg.cpp
@@ -936,6 +936,7 @@ int quark_cont::print_contractions(std::ostream& os, const std::string mode){
         os << std::endl << "//compute sink blocks and diagrams:\n{\n";
         indent+="\t";
         os << indent+"int tf, src;\n";
+        os << indent+"NODE0_PRINTF(\"Computing contractions for operator %d\",operator_id)\n;";
         os << indent << "for(tf=0; tf<lt; tf++) for(src=0; src<nsrc; src++){" << std::endl;
         indent+="\t";
         //set up temporary storage:
